b8aab35a83c14fd61754e285503635b7572d652f
Ryo Otwell <ytryo789@gmail.com>
2023-10-16 03:50:55 +0800

    Belajar object dictionary pada python

---
+++ object-dictionary/main.py
@@ -0,0 +1,10 @@
+"""
+Web Profile : https://ryodev.my.id
+Repo        : https://github.com/ryotwell/learn-python
+"""
+
+user = {"nama": "Ryo Otwell", "usia": 19, "status": "Mahasiswa"}
+
+print( f"Nama   : {user['nama']}" )
+print( f"Usia   : {user['usia']} Tahun" )
+print( f"Status : {user['status']}" )
